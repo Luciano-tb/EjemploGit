5cddc5e45104328effa24383788a42d09afda1e5
Luciano-tb <lucianotburg@gmail.com>
2024-08-19 14:31:19 -0300

    Agregar archivos de proyecto.

---
+++ EjemploGit.Consola/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EjemploGit.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejemplo de uso de Git");
+        }
+    }
+}
